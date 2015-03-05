82fc9c54d66dcb5f36d953ecc4dbdc9a0de0ea82
jayaramimca <jayaramimca@gmail.com>
2015-03-06 05:09:28 +0530

    fixed #4 #7 password issue and modified the autodeployment in structure format

--- autodeploy.sh
+++ autodeploy.sh
@@ -12,14 +12,12 @@
 # cp -rv ./autodeploy.sh /home/kumarprd
 usage(){
 
-    echo "Usage : $0 -p <project_type> -j <java_version> -r <repo> -b <branch> -jv <Java_version> -nj
+    echo "Usage : $0 -p <project_type> -j <java_version> -r <repo> -b <branch>
     E.g. : ./autodeploy.sh -p java -j 1.8 -r uimirror/rtp.git -b devlop
     project_type: Specifies which type of Project it is, i.e [java, other], default is java
     java_version: If Project type is Java, which java version to use, default is 1.8
     repo : path too git repo (e.g. uimirror/rtp.git)
-    branch : devlop OR master (default is devlop if not mentioned)
-    Java_version: Specifies which version of Java to Use for deployment
-    nj: Specifies if its a non java project"
+    branch : devlop OR master (default is devlop if not mentioned)"
 }
 
 #Prints Bye Message before closing the Script
